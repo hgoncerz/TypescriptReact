3792cbf15f0f396e0d8af7db577f83994f4215fa
hgoncerz <hubertg489655@gmail.com>
2022-06-28 16:25:17 +0200

    create-react-app typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+let name: string = "Piyush";
+let age: number;
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+role = [5, "x"];
+
+type Person = {
+  name: string;
+  age?: number;
+};
+
+let person: Person = {
+  name: "xd",
+};
+
+let lotsOfPeople: Person[];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>hello world</div>;
 }
 
 export default App;
