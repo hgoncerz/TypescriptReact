f7405f72f93d37195898810aee37f4365aab50ba
hgoncerz <hubertg489655@gmail.com>
2022-06-28 16:38:47 +0200

    basic syntax type interface ...

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,56 @@ import React from "react";
 import "./App.css";
 
 let name: string = "Piyush";
-let age: number;
+let age: number | string;
 let isStudent: boolean;
 let hobbies: string[];
 let role: [number, string];
 
-role = [5, "x"];
+let printName: (name: string) => never;
 
-type Person = {
+let personName: unknown;
+
+interface Person2 {
   name: string;
   age?: number;
-};
+}
 
-let person: Person = {
-  name: "xd",
-};
+// type X = {
+//   a: string;
+//   b: number;
+// };
+
+// type Y = {
+//   c: string;
+//   d: number;
+// };
+
+// let y: Y = {
+//   c: "efsdf",
+//   d: 42,
+// };
+
+interface Person {
+  name: string;
+  age?: number;
+}
+
+interface Guy extends Person {
+  profession: string;
+}
+
+role = [5, "x"];
+
+age = 5;
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// };
+
+// let person: Person = {
+//   name: "xd",
+// };
 
 let lotsOfPeople: Person[];
 
