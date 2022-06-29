15c806e97903ea5eadf300fb7fce7bfa34b21f26
hgoncerz <hubertg489655@gmail.com>
2022-06-29 11:37:37 +0200

    added complete ui input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import InputField from "./components/InputField";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
   return (
     <div className="flex items-center flex-col w-full h-screen">
       <span className="uppercase md:text-6xl text-2xl md:mt-8 mt-3 text-white text-center z-10">
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -2,15 +2,21 @@ import React from "react";
 
 const InputField = () => {
   return (
-    <form className="flex w-11/12 relative align-middle">
-      <input
-        type="input"
-        placeholder="Enter a task"
-        className="w-full border border-gray-300 px-3 py-2 shadow-sm rounded-lg focus:ring-indigo-500 "
-      />
-      <button type="submit" className="absolute w-2/4 h-2/4 mt-3">
-        Go
-      </button>
+    <form className="flex">
+      <div className="content-center">
+        <input
+          type="input"
+          placeholder="Enter a task"
+          className="border border-gray-300 px-3 py-2 shadow-sm rounded-lg 
+        focus:ring-indigo-500 my-2 md:w-96 w-56"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-3 rounded ml-1"
+        >
+          Go
+        </button>
+      </div>
     </form>
   );
 };
