2f04309fee05a84f0b75a97a851d8bf6c37ec1d3
hgoncerz <hubertg489655@gmail.com>
2022-06-30 10:34:53 +0200

    added add remove and edit

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Todo } from "../model";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
 import { MdDone } from "react-icons/md";
+import TodoList from "./TodoList";
 
 type Props = {
   todo: Todo;
@@ -10,19 +11,78 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   return (
-    <form className="flex w-4/12 rounded-md p-5 mt-4 bg-indigo-50">
-      <span className="flex-1 p-1 border-none text-sm focus:outline-none">
-        {todo.todo}
-      </span>
+    <form
+      className="flex rounded-md p-5 mt-4 bg-indigo-50 mx-2 flex-wrap justify-between"
+      onSubmit={(e) => handleEdit(e, todo.id)}
+    >
+      {edit ? (
+        <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
+        <s className="flex p-1 border-none text-sm focus:outline-none  ">
+          {todo.todo}
+        </s>
+      ) : (
+        <span className="flex p-1 border-none text-sm focus:outline-none  ">
+          {todo.todo}
+        </span>
+      )}
+
       <div className="flex">
-        <span className="ml-2.5 text-2xl cursor-pointer">
+        <span
+          className="ml-2.5 text-2xl cursor-pointer mx-2"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
-        <span className="ml-2.5 text-2xl cursor-pointer">
+        <span
+          className="ml-2.5 text-2xl cursor-pointer"
+          onClick={() => handleDelete(todo.id)}
+        >
           <AiFillDelete />
         </span>
-        <span className="ml-2.5 text-2xl cursor-pointer">
+        <span
+          className="ml-2.5 text-2xl cursor-pointer"
+          onClick={() => handleDone(todo.id)}
+        >
           <MdDone />
         </span>
       </div>
